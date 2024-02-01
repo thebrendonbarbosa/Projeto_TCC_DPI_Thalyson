2a36fab94f93d1531610cdb0af4fbd896ff65ac4
Brendon Barbosa <83964515+thebrendonbarbosa@users.noreply.github.com>
2024-02-01 14:21:28 -0300

    Att do repositorio

---
+++ softwares/simulador/__init__.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------
+# Universidade Federal do Pará
+# Campus Universitário de Tucuruí
+# Faculdade de Engenharia Elétrica
+# -----------------------------------------------------
+#
+# Laboratório Virtual Sistemas Dinâmicos e Controle
+# Tema: Simulação Duplo Pêndulo Invertido
+# Autor: Thalsyon Silva
+# Orientadores: Prof. Dr: Raphael Teixeira
+#               
+#
+# Data: 2024
+#  ----------------------------------------------------
+#
+from .graficos_duplopendulo import Graficos                  # noqa: F401
+from .animacao_duplopendulo import AnimacaoAeropendulo       # noqa: F401
+from .simulador import Simulador                            # noqa: F401
+
+
+__version__ = "0.5.0"
