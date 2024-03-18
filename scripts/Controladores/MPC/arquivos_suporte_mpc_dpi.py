e33d5c33ea166ba5fb0e5ba00ac0bb39a44df49f
T.Brendon Barsi <83964515+thebrendonbarbosa@users.noreply.github.com>
2024-03-18 19:45:18 +0000

    Att

--- scripts/Controladores/MPC/arquivos_suporte_mpc_dpi.py
+++ scripts/Controladores/MPC/arquivos_suporte_mpc_dpi.py
@@ -128,3 +128,123 @@ def espaco_de_estados(self):
     Dd=D
 
     return Ad,Bd,Cd,Dd
+
+def mpc_otimizacao(self, Ad, Bd, Cd, Dd, hz):
+    '''Esta função cria as matrizes compactas para Controle Preditivo de Modelo'''
+    # db - barra dupla
+    # dbt - barra dupla transposta
+    # dc - duplo circumflexo
+
+    A_aug=np.concatenate((Ad,Bd),axis=1)
+    temp1=np.zeros((np.size(Bd,1),np.size(Ad,1)))
+    temp2=np.identity(np.size(Bd,1))
+    temp=np.concatenate((temp1,temp2),axis=1)
+
+    A_aug=np.concatenate((A_aug,temp),axis=0)
+    B_aug=np.concatenate((Bd,np.identity(np.size(Bd,1))),axis=0)
+    C_aug=np.concatenate((Cd,np.zeros((np.size(Cd,0),np.size(Bd,1)))),axis=1)
+    D_aug=Dd
+
+
+    Q=self.constantes['Q']
+    S=self.constantes['S']
+    R=self.constantes['R']
+
+    CQC=np.matmul(np.transpose(C_aug),Q)
+    CQC=np.matmul(CQC,C_aug)
+
+    CSC=np.matmul(np.transpose(C_aug),S)
+    CSC=np.matmul(CSC,C_aug)
+
+    QC=np.matmul(Q,C_aug)
+    SC=np.matmul(S,C_aug)
+
+
+    Qdb=np.zeros((np.size(CQC,0)*hz,np.size(CQC,1)*hz))
+    Tdb=np.zeros((np.size(QC,0)*hz,np.size(QC,1)*hz))
+    Rdb=np.zeros((np.size(R,0)*hz,np.size(R,1)*hz))
+    Cdb=np.zeros((np.size(B_aug,0)*hz,np.size(B_aug,1)*hz))
+    Adc=np.zeros((np.size(A_aug,0)*hz,np.size(A_aug,1)))
+
+    for i in range(0,hz):
+        if i == hz-1:
+            Qdb[np.size(CSC,0)*i:np.size(CSC,0)*i+CSC.shape[0],np.size(CSC,1)*i:np.size(CSC,1)*i+CSC.shape[1]]=CSC
+            Tdb[np.size(SC,0)*i:np.size(SC,0)*i+SC.shape[0],np.size(SC,1)*i:np.size(SC,1)*i+SC.shape[1]]=SC
+        else:
+            Qdb[np.size(CQC,0)*i:np.size(CQC,0)*i+CQC.shape[0],np.size(CQC,1)*i:np.size(CQC,1)*i+CQC.shape[1]]=CQC
+            Tdb[np.size(QC,0)*i:np.size(QC,0)*i+QC.shape[0],np.size(QC,1)*i:np.size(QC,1)*i+QC.shape[1]]=QC
+
+        Rdb[np.size(R,0)*i:np.size(R,0)*i+R.shape[0],np.size(R,1)*i:np.size(R,1)*i+R.shape[1]]=R
+
+        for j in range(0,hz):
+            if j<=i:
+                Cdb[np.size(B_aug,0)*i:np.size(B_aug,0)*i+B_aug.shape[0],np.size(B_aug,1)*j:np.size(B_aug,1)*j+B_aug.shape[1]]=np.matmul(np.linalg.matrix_power(A_aug,((i+1)-(j+1))),B_aug)
+
+        Adc[np.size(A_aug,0)*i:np.size(A_aug,0)*i+A_aug.shape[0],0:0+A_aug.shape[1]]=np.linalg.matrix_power(A_aug,i+1)
+
+    Hdb=np.matmul(np.transpose(Cdb),Qdb)
+    Hdb=np.matmul(Hdb,Cdb)+Rdb
+
+    temp=np.matmul(np.transpose(Adc),Qdb)
+    temp=np.matmul(temp,Cdb)
+
+    temp2=np.matmul(-Tdb,Cdb)
+    Fdbt=np.concatenate((temp,temp2),axis=0)
+
+    return Hdb,Fdbt,Cdb,Adc
+
+def novos_estados_malha_aberta(self, estados, U1):
+    '''Esta função calcula o novo vetor de estado para uma amostra posterior'''
+
+    # Constantes
+    m = self.constantes['m']
+    m1 = self.constantes['m1']
+    m2 = self.constantes['m2']
+    l1 = self.constantes['l1']
+    l2 = self.constantes['l2']
+    g = self.constantes['g']
+    f0 = self.constantes['f0']
+    f1 = self.constantes['f1']
+    f2 = self.constantes['f2']
+    J1 = self.constantes['J1']
+    J2 = self.constantes['J2']
+    Ts = self.constantes['Ts']
+    x_dot=self.constantes['x_dot']
+
+    estados_atual=estados
+    novos_estados=estados_atual
+
+    X = estados_atual[0]
+    phi1 = estados_atual[1]
+    phi2 =estados_atual[2]
+    X_dot = estados_atual[3]
+    phi1_dot = estados_atual[4]
+    phi2_dot = estados_atual[5]
+
+    sub_loop = 30   # Fatia Ts em 30 pedaços
+    for i in range(0,sub_loop):
+        # Computa as derivadas dos estados
+        X_dot= 0
+        phi1_dot = 0
+        phi2_dot = 0
+        X_dot_dot = 0
+        phi1_dot_dot = 0
+        phi2_dot_dot = 0
+
+        # Atualiza os valores de estado com novas derivadas de estado
+        X = X+ X_dot*Ts/sub_loop
+        phi1 = phi1 + phi1_dot*Ts/sub_loop
+        phi2 =phi2 + phi2_dot*Ts/sub_loop
+        X_dot = X_dot+ X_dot_dot*Ts/sub_loop
+        phi1_dot = phi1_dot + phi1_dot_dot*Ts/sub_loop
+        phi2_dot = phi2_dot + phi2_dot_dot*Ts/sub_loop
+
+    # Pegue os últimos estados
+    novos_estados[0]= X
+    novos_estados[1]=phi1
+    novos_estados[2]=phi2
+    novos_estados[3]= X_dot
+    novos_estados[4]= phi1_dot
+    novos_estados[5]= phi2_dot
+
+    return novos_estados
